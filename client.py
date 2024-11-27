91dc372fc828cda9fddfccfb06e1fe6185d78b04
mudit14224 <mudit.jindal14224@gmail.com>
2024-11-27 14:09:28 -0600

    Added Initial RAG code

--- client.py
+++ client.py
@@ -8,25 +8,43 @@ config.read('config.ini')
 
 # URL of the Lambda function
 LAMBDA_SERVER_URL = config['aws']['upload_url']
+LAMBDA_RAG_URL = config['aws']['rag_url']
 
-# File to upload
-FILE_PATH = './test_files/paper1.pdf'
-FILENAME = 'paper1.pdf'
+choice = int(input("Enter a Number> \n"))
 
-# Open the file and encode it in Base64
-with open(FILE_PATH, 'rb') as file:
-    file_content = base64.b64encode(file.read()).decode('utf-8')
+if choice == 1: 
+    # File to upload
+    FILE_PATH = './test_files/paper1.pdf'
+    FILENAME = 'paper1.pdf'
 
-# Send the POST request to the Lambda function
-response = requests.post(
-    LAMBDA_SERVER_URL,
-    params={'filename': FILENAME},  # Add the filename as a query string parameter
-    data=file_content,  # Base64-encoded file content
-    headers={'Content-Type': 'application/json'}  # Set content type
-)
+    # Open the file and encode it in Base64
+    with open(FILE_PATH, 'rb') as file:
+        file_content = base64.b64encode(file.read()).decode('utf-8')
 
-# Check the response from the Lambda function
-if response.status_code == 200:
-    print('Success:', response.json())
-else:
-    print('Error:', response.status_code, response.json())
+    # Send the POST request to the Lambda function
+    response = requests.post(
+        LAMBDA_SERVER_URL,
+        params={'filename': FILENAME},  # Add the filename as a query string parameter
+        data=file_content,  # Base64-encoded file content
+        headers={'Content-Type': 'application/json'}  # Set content type
+    )
+
+    # Check the response from the Lambda function
+    if response.status_code == 200:
+        print('Success:', response.json())
+    else:
+        print('Error:', response.status_code, response.json())
+
+if choice == 2:
+    print("Performing RAG")
+    user_query = "Hello!"
+    response = requests.get(
+        LAMBDA_RAG_URL, 
+        params={'query': user_query}
+    )
+    if response.status_code == 200:
+        print("Response received:")
+        print(response.json())  # Parse and print the JSON response
+    else:
+        print(f"Error: Received status code {response.status_code}")
+        print(response.json())
