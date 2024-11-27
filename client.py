fdb858009a55a107c32a022d7c3c908464cdd56b
mudit14224 <mudit.jindal14224@gmail.com>
2024-11-27 13:15:45 -0600

    Added lambda function to upload files to S3

--- client.py
+++ client.py
@@ -1,17 +1,26 @@
 import requests
+import base64
 
-# URL of the Flask server
-FLASK_SERVER_URL = 'http://127.0.0.1:5000/upload'
+# URL of the Lambda function
+LAMBDA_SERVER_URL = 'https://du4h657am6lbmobtw3amspowqa0eotqz.lambda-url.us-east-1.on.aws/'
 
 # File to upload
 FILE_PATH = './test_files/paper1.pdf'
+FILENAME = 'paper1.pdf'
 
-# Open the file in binary mode and send it as part of the POST request
+# Open the file and encode it in Base64
 with open(FILE_PATH, 'rb') as file:
-    files = {'file': file}
-    response = requests.post(FLASK_SERVER_URL, files=files)
+    file_content = base64.b64encode(file.read()).decode('utf-8')
 
-# Check the response from the server
+# Send the POST request to the Lambda function
+response = requests.post(
+    LAMBDA_SERVER_URL,
+    params={'filename': FILENAME},  # Add the filename as a query string parameter
+    data=file_content,  # Base64-encoded file content
+    headers={'Content-Type': 'application/json'}  # Set content type
+)
+
+# Check the response from the Lambda function
 if response.status_code == 200:
     print('Success:', response.json())
 else:
